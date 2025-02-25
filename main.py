75bd73547ceee13b4b1fd19f8bf405fb716c91f8
Antua <suleykin2005@mail.ru>
2025-02-25 15:46:00 +0300

    базовое первое задание

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from math import cos, tan, log
+from math import log10
 
-def main(y, x, z):
-    first = pow(tan(pow(y, 3) + pow(z, 2)), 7) - pow(x, 2)
-    sec = 97 * pow((50 * y + 10 * pow(y, 2) + pow(x, 3)), 2)
-    third = 92 * pow(cos(z), 4)
-    fourth = pow((52 + pow(y, 3) + 89 * pow(z, 2)), 3)
-    fifth = 10 * pow(log(0.05 + pow(x, 2) / 38 + y, 2), 5)
-    return first - ((sec - third) / (fourth - fifth))
+
+def main(a, b):
+    result = 0
+    for j in range(1, b + 1):
+        for i in range(1, a + 1):
+            result = result + (8 * pow(j, 5) + 98 *
+                               pow(log10(i), 7) + 64 * pow(j, 4))
+    for c in range(1, a + 1):
+        result = result + pow(c, 5) + 6 * c
+    return result
+
+main(3, 4)
