0fe0b9ed9538c182a0390b34b68bd6acc7e880b6
Antua <suleykin2005@mail.ru>
2025-02-25 15:27:35 +0300

    test

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from math import atan
 def main(n):
     if n == 0:
         return -0.24
-    elif n == 1:
+    if n == 1:
         return 0.29
-    f = [0.0] * (n + 1)
-    f[0] = -0.24
-    f[1] = 0.29
+    a, b = -0.24, 0.29
     for i in range(2, n + 1):
-        f[i] = 34 * atan(f[i - 1]) ** 3 + f[i - 2]
-    return f[n]
+        c = 34 * pow(atan(b), 3) + a
+        a, b = b, c
+    return b
+
 
 print(main(5))
