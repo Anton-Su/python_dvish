038368ad2c50f95a1e2da0b55d2b11ff737f5207
Antua <suleykin2005@mail.ru>
2025-02-25 14:40:12 +0300

    test

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from math import cos, tan, log
+from math import atan
 
-def main(y, x, z):
-    first = pow(tan(pow(y, 3) + pow(z, 2)), 7) - pow(x, 2)
-    sec = 97 * pow((50 * y + 10 * pow(y, 2) + pow(x, 3)), 2)
-    third = 92 * pow(cos(z), 4)
-    fourth = pow((52 + pow(y, 3) + 89 * pow(z, 2)), 3)
-    fifth = 10 * pow(log(0.05 + pow(x, 2) / 38 + y, 2), 5)
-    return first - ((sec - third) / (fourth - fifth))
+
+def main(n):
+    if n == 0:
+        return -0.24
+    elif n == 1:
+        return 0.29
+    f = [0.0] * (n + 1)
+    f[0] = -0.24
+    f[1] = 0.29
+    for i in range(2, n + 1):
+        f[i] = 34 * atan(f[i - 1]) ** 3 + f[i - 2]
+    return f[n]
+
+print(main(5))
