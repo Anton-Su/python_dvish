55b262229064a4778e462c51fe3210359545a6eb
Antua <suleykin2005@mail.ru>
2025-03-08 20:31:06 +0300

    first_option

--- main.py
+++ main.py
@@ -2,16 +2,10 @@
 
 
 def main(H: set):
-    E = set()
-    alpha = set()
-    for i in H:
-        if 42 > i >= -48:
-            E.add(i // 7 - 4 * i)
-        peremenaya = 3 * i + i // 7
-        if ((peremenaya >= 91 or peremenaya < 22) and
-                (i >= 42 or i < -88)):
-            alpha.add(abs(peremenaya))
-    return len(E.union(alpha)) + sum(alpha)
+    E = {i // 7 - 4 * i for i in H if 42 > i >= -48}
+    X = {3 * i + i // 7 for i in H if i >= 42 or i < -88}
+    alpha = {abs(i) for i in X if i >= 91 or i < 22}
+    return len(E | alpha) + sum(alpha)
 
 
 
