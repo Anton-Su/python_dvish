3353b401d6b4d496fea6b160951348d275efed8a
Antua <suleykin2005@mail.ru>
2025-03-16 19:07:04 +0300

    just ifs = 1 решение

--- main.py
+++ main.py
@@ -1,21 +1,61 @@
-s = ({'SAS', 1966, 1985},
-     {'SAS', 1966, 1994},
-     {'SAS', 1966, 1974, 'MQL4'},
-     {'SAS', 1966, 1974, 'ASN.1'},
-     {'SAS', 1959, 1985, 'RED'},
-     {'SAS', 1959, 1985, 'PERL6'},
-     {'SAS', 1959, 1994, 'RED'},
-     {'SAS', 1959, 1994, 'PERL6'},
-     {'SAS', 1959, 1974},
-     {'X10', 1985},
-     {'X10', 1994},
-     {'X10', 1974},
-     {'YACC'})
+def x10(r):
+    if 1985 in r:
+        return 9
+    if 1994 in r:
+        return 10
+    if 1974 in r:
+        return 11
+
+
+def fun_1994(r):
+    if 'RED' in r:
+        return 6
+    if 'PERL6' in r:
+        return 7
+
+
+def fun_1985(r):
+    if 'RED' in r:
+        return 4
+    if 'PERL6' in r:
+        return 5
+
+
+def f_1959(r):
+    if 1985 in r:
+        return fun_1985(r)
+    if 1994 in r:
+        return fun_1994(r)
+    if 1974 in r:
+        return 8
+
+
+def f_1966(r):
+    if 1985 in r:
+        return 0
+    if 1994 in r:
+        return 1
+    if 1974 in r:
+        if 'MQL4' in r:
+            return 2
+        if 'ASN.1' in r:
+            return 3
+
+
+def sas(r):
+    if 1959 in r:
+        return f_1959(r)
+    if 1966 in r:
+        return f_1966(r)
 
 
 def main(r):
-    s1 = set(r)
-    return [i for i in range(len(s)) if not(len(s[i] - s1))][0]
+    if 'SAS' in r:
+        return sas(r)
+    if 'X10' in r:
+        return x10(r)
+    if 'YACC' in r:
+        return 12
 
 
-print(main([1966, 'ASN.1', 1994, 'X10', 'PERL6']))
+print(main([1959, 'ASN.1', 1985, 'SAS', 'PERL6']))
