94a37dfdd82c03ddd044f55965794fa30673e69c
Antua <suleykin2005@mail.ru>
2025-03-23 21:04:28 +0300

    единственное решение

--- main.py
+++ main.py
@@ -1,69 +1,16 @@
-def x10(r):
-    match r:
-        case _ if 1985 in r:
-            return 9
-        case _ if 1994 in r:
-            return 10
-        case _ if 1974 in r:
-            return 11
+bit_positions = {
+    'C1': (0, 7),
+    'C2': (7, 5),
+    'C3': (12, 7),
+    'C5': (21, 9)
+    }
 
 
-def fun_1994(r):
-    match r:
-        case _ if 'RED' in r:
-            return 6
-        case _ if 'PERL6' in r:
-            return 7
-
-
-def fun_1985(r):
-    match r:
-        case _ if 'RED' in r:
-            return 4
-        case _ if 'PERL6' in r:
-            return 5
-
-
-def f_1959(r):
-    match r:
-        case _ if 1985 in r:
-            return fun_1985(r)
-        case _ if 1994 in r:
-            return fun_1994(r)
-        case _ if 1974 in r:
-            return 8
-
-
-def f_1966(r):
-    match r:
-        case _ if 1985 in r:
-            return 0
-        case _ if 1994 in r:
-            return 1
-        case _ if 1974 in r:
-            match r:
-                case _ if 'MQL4' in r:
-                    return 2
-                case _ if 'ASN.1' in r:
-                    return 3
-
-
-def sas(r):
-    match r:
-        case _ if 1959 in r:
-            return f_1959(r)
-        case _ if 1966 in r:
-            return f_1966(r)
-
-
-def main(r):
-    match r:
-        case _ if 'SAS' in r:
-            return sas(r)
-        case _ if 'X10' in r:
-            return x10(r)
-        case _ if 'YACC' in r:
-            return 12
-
-
-print(main([1959, 'ASN.1', 1985, 'SAS', 'PERL6']))
+def main(spisok: list) -> int:
+    result = 0
+    for name, value in spisok:
+        shift, size = bit_positions[name]
+        maska = (1 << size) - 1
+        chiclo = (int(value) & maska) << shift
+        result |= chiclo
+    return result
