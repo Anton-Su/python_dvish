a9ac8a44d271770cd69afe5243028b4d4c56a4af
Antua <suleykin2005@mail.ru>
2025-03-27 01:41:47 +0300

    единственное решение (3)

--- main.py
+++ main.py
@@ -8,10 +8,15 @@ bit_positions = {
 
 def main(spisok: list) -> int:
     binary_string = ['0'] * 30
-    for name, value in spisok:
-        start, length = bit_positions[name]
-        bin_value = bin(int(value))[2:][::-1].ljust(length, '0')[-length:]
-        binary_string[start:start + length] = bin_value  # Заменяем нужные биты в строке
+    first, sec, third, fifth = spisok
+    bin_value = bin(int(first[1]))[2:][::-1].ljust(7, '0')[-7:]
+    binary_string[0:7] = bin_value
+    bin_value = bin(int(sec[1]))[2:][::-1].ljust(5, '0')[-5:]
+    binary_string[7:12] = bin_value
+    bin_value = bin(int(third[1]))[2:][::-1].ljust(7, '0')[-7:]
+    binary_string[12:19] = bin_value
+    bin_value = bin(int(fifth[1]))[2:][::-1].ljust(9, '0')[-9:]
+    binary_string[21:30] = bin_value
     return int(''.join(binary_string[::-1]), 2)
 
 
