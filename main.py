5cddf516009e3ac337952bfccfb6fd05030ae92b
Antua <suleykin2005@mail.ru>
2025-03-31 01:20:48 +0300

    первое решение

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from math import atan
+dictionary = {}
+
+def main(stroka: str):
+    text = ''.join(stroka.split('\n')).strip('done.>>').replace(' ', '')
+    text = text.split('@')[1:]
+    text = [i.rstrip(';done.donew') for i in text]
+    for stroka in text:
+        stroka = stroka.replace("'", "")
+        key, value = stroka.split(':')
+        value = value.strip('#').strip('()').split(';')
+        dictionary[key] = value
+    return dictionary
 
 
-def main(n):
-    if n == 0:
-        return -0.24
-    if n == 1:
-        return 0.29
-    else:
-        return 34 * pow(atan(main(n-1)), 3) + main(n-2)
+dannie = "<< do new @'avea' : #('isve_803'; 'orisor' ; 'lemaat' ); done. do new\n@'dimaon_947' : #( 'istele'; 'dior' ); done. do new @'geceso_976'\n:#('quso_200' ; 'ratian_166' ); done. do new @'diorve_14' :#( 'tevere'\n; 'onte_700' ); done.>>"
 
-main(5)
+print(main(dannie))
