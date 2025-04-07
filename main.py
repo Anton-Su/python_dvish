932982e8daed7b24835038a5da5dd75c41419914
Antua <suleykin2005@mail.ru>
2025-04-07 04:06:40 +0300

    вторая попытка решения

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 dictionary = {}
 
-def main(massiv: list) -> list:
-    massiv = list(zip(*massiv))[1:]
-    massiv = [list(i) for i in massiv]
-    massiv[0] = [1 if massiv[0][i] == 'true' else 0
-                 for i in range(len(massiv[0]))]
-    sec = []
-    for i in range(len(massiv[1])):
-        rasdelenie = massiv[1][i].split('|')
-        massiv[1][i] = rasdelenie[0].split(' ')[0]
-        sec.append('.'.join(rasdelenie[1].split('.')[::-1]).
-                   replace('.', '/'))
-    massiv.append(sec)
-    massiv = list(zip(*massiv))
-    massiv = sorted(massiv, key=lambda x: (x[0], x[1]))
-    return list(zip(*massiv))
+def main(data: list) -> list:
+    cleaned = []
+    for row in data:
+        flag = 1 if row[1] == 'true' else 0
+        fio, raw_date = row[2].split('|')
+        fio = fio.split(' ')[0]  # Берем только первую часть ФИО
+        date = ('.'.join(raw_date.split('.')[::-1]).
+                replace('.', '/'))
+        cleaned.append([flag, fio, date])
+    cleaned.sort(key=lambda x: (x[0], x[1]))
+    result = list(map(list, zip(*cleaned)))
+    return result
+
 
 
 dannie = [[None, 'true', 'Шицамяк Захар|17.04.00'], [None, 'true', 'Натий Демид|23.04.00'], [None, 'true', 'Фуфимиди Елисей|05.01.99'], [None, 'false', 'Зилолиди Иван|09.02.03']]
