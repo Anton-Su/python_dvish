ce6d400b2f5c1b425af2cab904154a4e2bcdcbec
Antua <suleykin2005@mail.ru>
2025-04-07 10:53:49 +0300

    вторая попытка решения - 1-ое решение

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-dictionary = {}
-
 def main(data: list) -> list:
     cleaned = []
     for row in data:
-        flag = 1 if row[1] == 'true' else 0
+        flag = '1' if row[1] == 'true' else '0'
         fio, raw_date = row[2].split('|')
-        fio = fio.split(' ')[0]  # Берем только первую часть ФИО
+        fio = fio.split(' ')[0]
         date = ('.'.join(raw_date.split('.')[::-1]).
                 replace('.', '/'))
         cleaned.append([flag, fio, date])
-    cleaned.sort(key=lambda x: (x[0], x[1]))
+    cleaned.sort(key=lambda x: (x[1]))
     result = list(map(list, zip(*cleaned)))
     return result
 
 
 
-dannie = [[None, 'true', 'Шицамяк Захар|17.04.00'], [None, 'true', 'Натий Демид|23.04.00'], [None, 'true', 'Фуфимиди Елисей|05.01.99'], [None, 'false', 'Зилолиди Иван|09.02.03']]
+dannie = [[None, 'false', 'Домесук Захар|09.12.00'], [None, 'true', 'Водагли Арсен|02.12.03'], [None, 'true', 'Казугук Игнат|15.10.04'], [None, 'false', 'Гофов Самир|16.01.03']]
 print(main(dannie))
